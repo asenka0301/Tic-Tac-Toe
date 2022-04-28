6fc4a3bd8fd38899e6c4230e4657cb85e60a4381
Anastasia Butina <asenka0301@gmail.com>
2022-04-28 23:35:14 +0700

    Backup

--- app.js
+++ app.js
@@ -1,25 +1,108 @@
-const startBtn = document.querySelector('.btn');
-const menu = document.querySelector('.menu');
-const controlsBtn = document.querySelector('.controls');
-const playerType = document.querySelectorAll('.controls .btn');
-const dashboard = document.querySelector('.dashboard');
-console.log(dashboard);
+function StartGame(startBtn) {
+   const controlsBtn = document.querySelector('.controls');
+   const menu = document.querySelector('.menu');
 
+   controlsBtn.style.display = 'flex';
+   controlsBtn.style.opacity = '1';
 
-startBtn.addEventListener('click', function() {
-    menu.style.position = 'absolute';
-    menu.style.animation = 'menu 1.5s';
-    menu.style.top = '5%';
-    startBtn.style.display = 'none';
-    controlsBtn.style.opacity = '1';
+   menu.style.position = 'absolute';
+   menu.style.animation = 'menu 1.5s';
+   menu.style.top = '10%';
+
+   startBtn.style.display = 'none';
+}
+function FirstStart(){
+   const startBtn = document.querySelector('.start_btn');
+   startBtn.addEventListener('click', function () {
+      StartGame(startBtn);
+   });
+}
+FirstStart();
+
+const gameModes = document.querySelectorAll('.controls .btn');
+gameModes.forEach((gameMode) => {
+   gameMode.addEventListener('click', () => {
+      const controlsBtn = document.querySelector('.controls');
+      controlsBtn.style.display = 'none';
+
+      const dashboard = document.querySelector('.dashboard');
+      dashboard.classList.add('dashboard-visible');
+     
+      if(gameMode.id === 'btn-computer') {
+         const player2 = document.querySelector('.player_2');
+         player2.textContent = 'Computer (O)'
+      }
+   });
 });
 
+const Player = (sign) => {
+
+   const getSign = () => {
+      return sign;
+   }
+
+   return {getSign};
+
+}
+
+const gameBoard = (() => {
+   
+   const board = [
+      '', '', '',
+      '', '', '',
+      '', '', '',
+   ];
+   
+   function fillCell(cell, sign) {
+      const i = cell.dataset.cell; 
+      board[i] = sign;
+      cell.textContent = sign;
+      
+      console.log(board);
+   }
+
+   function cleanBoard() {
+      // 
+   };
+
+   function isCellEmpty(i) {
+      if (board[i].length == 0) {
+         return true;
+      }
+      return false;
+   };
+
+   return {
+      fillCell,
+   }
+})()
+
+const displayControll = ( () => {
+   
 
 
-playerType.forEach(element => {
-    element.addEventListener('click', function(){
-        dashboard.classList.add('dashboard-visible');
-        
+})()
 
-    });
-});
+const gameControll = (() => {
+
+   const Player1 = Player('X');
+   const Player2 = Player('O');
+   let round = 1;
+   let gameIsOver = false;
+   const board = document.getElementById('gird');
+
+
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+
